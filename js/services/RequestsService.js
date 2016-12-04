d848ef3d0c665b440c3e24fb7f17ccbb87b8ae01
luchtrix58CF <lalito5828@gmail.com>
2016-12-04 15:31:14 -0600

    Update RequestsService.js

--- js/services/RequestsService.js
+++ js/services/RequestsService.js
@@ -10,18 +10,19 @@
         var base_url = 'http://192.168.0.104:3000/';
 
         function register(device_token){
-
+            alert("Registro "+device_token);
             var deferred = $q.defer();
             $ionicLoading.show();
 
             $http.post(base_url + '/register', {'device_token': device_token})
                 .success(function(response){
-
+                    alert("Pues segun el post jalo...???");
                     $ionicLoading.hide();
                     deferred.resolve(response);
 
                 })
                 .error(function(data){
+                    alert("E R R O R"+data);
                     deferred.reject();
                 });
 
